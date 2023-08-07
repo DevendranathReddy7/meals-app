3cc6d7b63f789626c5199929fd06f5ba135b9b7a
Devendra <devendranathreddy54@gmail.com>
2023-08-07 21:36:34 +0530

    new chanes

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -11,18 +11,40 @@ const MenuBar = () => {
                     <h3>React Meals</h3>
                 </div>
                 <div className="quickLinks">
-                    <div >
-                        <img src={search} alt="search" className='icons' />
-                        <span className='icon-name'>Search</span>
+                    <div>
+                        <div className='icon-shape'>
+                            <img src={search} alt="search" className='icons' />
+                        </div>
+                        <div className='icon-shape icon-name'>
+                            <div>
+                                <p>Search</p>
+                            </div>
+                        </div>
                     </div>
-                    <div >
-                        <img src={profile} alt="profile" className='icons' />
-                        <span className='icon-name'>Sign-in</span>
+
+                    <div>
+                        <div className='icon-shape'>
+                            <img src={profile} alt="profile" className='icons' />
+                        </div>
+                        <div className='icon-shape icon-name'>
+                            <div>
+                                <p>Sign-in</p>
+                            </div>
+                        </div>
                     </div>
-                    <div >
-                        <img src={cart} alt="cart" className='icons' />
-                        <span className='icon-name'>Cart</span>
+
+                    <div>
+                        <div className='icon-shape'>
+                            <img src={cart} alt="cart" className='icons' />
+                        </div>
+                        <div className='icon-shape icon-name'>
+                            <div>
+                                <p>Cart</p>
+                            </div>
+                        </div>
                     </div>
+
+
 
                 </div>
             </div>
