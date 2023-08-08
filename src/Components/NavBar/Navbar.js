492a02c2921cae235c98c13f01cbe49c64b14579
Devendra <devendranathreddy54@gmail.com>
2023-08-08 21:10:25 +0530

    fixed navbar css

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -3,7 +3,7 @@ import search from '../../assests/icons8-search-24.png'
 import profile from '../../assests/icons8-male-user-24.png'
 import cart from '../../assests/icons8-cart-24.png'
 
-const MenuBar = () => {
+const MenuBar = (props) => {
     return (
         <>
             <div className="nav-container">
@@ -11,36 +11,30 @@ const MenuBar = () => {
                     <h3>React Meals</h3>
                 </div>
                 <div className="quickLinks">
-                    <div>
+                    <div className='icon-div'>
                         <div className='icon-shape'>
                             <img src={search} alt="search" className='icons' />
                         </div>
-                        <div className='icon-shape icon-name'>
-                            <div>
-                                <p>Search</p>
-                            </div>
+                        <div className='icon-name'>
+                            <p>Search</p>
                         </div>
                     </div>
 
-                    <div>
+                    <div className='icon-div'>
                         <div className='icon-shape'>
                             <img src={profile} alt="profile" className='icons' />
                         </div>
-                        <div className='icon-shape icon-name'>
-                            <div>
-                                <p>Sign-in</p>
-                            </div>
+                        <div className='icon-name'>
+                            <p>Sign-in</p>
                         </div>
                     </div>
 
-                    <div>
+                    <div className='icon-div'>
                         <div className='icon-shape'>
                             <img src={cart} alt="cart" className='icons' />
                         </div>
-                        <div className='icon-shape icon-name'>
-                            <div>
-                                <p>Cart</p>
-                            </div>
+                        <div className='icon-name'>
+                            <p>Cart {props.count || undefined}</p>
                         </div>
                     </div>
 
