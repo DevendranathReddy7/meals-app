73d06542d730e7d82299acbe5ea85e841be96274
Devendra <devendranathreddy54@gmail.com>
2023-08-09 06:48:13 +0530

    optimised the code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import Navbar from './Components/NavBar/Navbar.js';
 import HomeScreen from './Components/Restaurent/HomeScreen';
+
+const items = [
+  {
+    "name": "Idly [3 Pieces]",
+    "price": "90/-",
+    "desc": "Idli is made from ground rice or rice flour mixed with ground urad dal, salt.",
+  },
+  {
+    "name": "Ghee Dosa",
+    "price": "110/-",
+    "desc": "Served with Palli Chutney and Allam Chutney.",
+  },
+  {
+    "name": "Mysore Bajji [4 Pieces]",
+    "price": "90/-",
+    "desc": "This mouth-watering snack is prepared with rice flour, baking soda, onion, all-purpose flour and ginger.",
+  }
+]
+
 function App() {
 
   return (
     <div className='App'>
       <Navbar />
-      <HomeScreen />
+      <HomeScreen items={items} />
     </div>
   );
 }
--- src/Components/Restaurent/HomeScreen.js
+++ src/Components/Restaurent/HomeScreen.js
@@ -5,40 +5,22 @@ const HomeScreen = (props) => {
 
     return (
         <>
-
             <div className="dish-container">
-                <div className='item'>
-                    <div className="dish-item">
-                        <h4>Idly [3 Pieces]</h4>
-                        <h4>90/-</h4>
-                        <p>Idli is made from ground rice or rice flour mixed with ground urad dal, salt.</p>
+                {props.items.map(eachItem =>
+                    <div >
+                        <div className='item'>
+                            <div className="dish-item">
+                                <h4>{eachItem.name}</h4>
+                                <h4>{eachItem.price}</h4>
+                                <p>{eachItem.desc}</p>
+                            </div>
+                            <div>
+                                <AddButton />
+                            </div>
+                        </div>
+                        <hr />
                     </div>
-                    <div>
-                        <AddButton />
-                    </div>
-                </div>
-                <hr />
-                <div className='item'>
-                    <div className="dish-item">
-                        <h4>Ghee Dosa</h4>
-                        <h4>110/-</h4>
-                        <p>Served with Palli Chutney and Allam Chutney.</p>
-                    </div>
-                    <div>
-                        <AddButton />
-                    </div>
-                </div>
-                <hr />
-                <div className='item'>
-                    <div className="dish-item">
-                        <h4>Mysore Bajji [4 Pieces]</h4>
-                        <h4>90/-</h4>
-                        <p>This mouth-watering snack is prepared with rice flour, baking soda, onion, all-purpose flour and ginger.</p>
-                    </div>
-                    <div>
-                        <AddButton />
-                    </div>
-                </div>
+                )}
             </div>
         </>
     )
